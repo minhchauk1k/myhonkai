8d4fcafaf26a7218cf997dde32e8f2bd2bda1666
minhchauk1k <5751071002@st.utc2.edu.vn>
2020-04-10 20:11:12 +0700

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ValkyriesComponent } from './valkyries/valkyries.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { GopYComponent } from './gop-y/gop-y.component';
 
 @NgModule({
   imports:      [ BrowserModule ,RouterModule.forRoot([
     {path:"",component:WelcomeComponent},
-    {path:"valkyries",component:ValkyriesComponent}
+    {path:"valkyries",component:ValkyriesComponent},
+    {path:"gop-y",component:GopYComponent}
   ])],
-  declarations: [ AppComponent, TopBarComponent, ValkyriesComponent, WelcomeComponent ],
+  declarations: [ AppComponent, TopBarComponent, ValkyriesComponent, WelcomeComponent, GopYComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
