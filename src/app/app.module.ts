246732c9e10b4320b14cd502c2661f54a22670e1
minhchauk1k <5751071002@st.utc2.edu.vn>
2020-04-10 17:42:26 +0700

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { RouterModule } from "@angular/router";
+import { Routes,RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ValkyriesComponent } from './valkyries/valkyries.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,RouterModule.forRoot([
+  imports:      [ BrowserModule ,RouterModule.forRoot([
     {path:"",component:AppComponent},
     {path:"valkyries",component:ValkyriesComponent}
   ])],
