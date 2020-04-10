026e3268b2f3d4e48162e4d3ff373bd957389639
minhchauk1k <5751071002@st.utc2.edu.vn>
2020-04-10 17:59:51 +0700

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { Routes,RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ValkyriesComponent } from './valkyries/valkyries.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   imports:      [ BrowserModule ,RouterModule.forRoot([
-    {path:"",component:AppComponent},
+    {path:"",component:WelcomeComponent},
     {path:"valkyries",component:ValkyriesComponent}
   ])],
-  declarations: [ AppComponent, TopBarComponent, ValkyriesComponent ],
+  declarations: [ AppComponent, TopBarComponent, ValkyriesComponent, WelcomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
