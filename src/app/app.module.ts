e41414a2f3f58e26470b5fdaac2c20678f273ba3
minhchauk1k <5751071002@st.utc2.edu.vn>
2020-04-10 17:10:27 +0700

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { RouterModule } from "@angular/router";
+
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ValkyriesComponent } from './valkyries/valkyries.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TopBarComponent, ValkyriesComponent ],
+  imports:      [ BrowserModule, FormsModule ,RouterModule.forRoot([
+    
+  ])],
+  declarations: [ AppComponent, TopBarComponent, ValkyriesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
