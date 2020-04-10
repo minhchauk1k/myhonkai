5bceb0c5163d041bfc2394b07c5d20fd237f6ff6
minhchauk1k <5751071002@st.utc2.edu.vn>
2020-04-10 17:07:50 +0700

    2 files modified, 2 added

---
+++ src/app/valkyries/valkyries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-valkyries',
+  templateUrl: './valkyries.component.html',
+  styleUrls: ['./valkyries.component.css']
+})
+export class ValkyriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
